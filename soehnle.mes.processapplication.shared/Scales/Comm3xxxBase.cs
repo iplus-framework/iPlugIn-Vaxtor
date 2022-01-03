3077231c0f3b5e8d4aed624409eebc1ce8521f3c
LisakMn <wingermaro@gmail.com>
2022-01-03 16:33:40 +0100

    Added Structure for PanPekPi

--- soehnle.mes.processapplication.shared/Scales/Comm3xxxBase.cs
+++ soehnle.mes.processapplication.shared/Scales/Comm3xxxBase.cs
@@ -12,15 +12,65 @@ namespace soehnle.mes.processapplication
         #region Methods
         public bool ReadWeightData(TcpClient channel, out string result)
         {
+            
             result = null;
-            return false;
+            if (channel == null || !channel.Connected)
+                return false;
+            try
+            {
+                    NetworkStream stream = channel.GetStream();
+                    if (stream == null)
+                        return false;
+                    if (stream.CanRead && stream.DataAvailable)
+                    {
+                        byte[] myReadBuffer = new byte[1024];
+                        StringBuilder myCompleteMessage = new StringBuilder();
+                        int numberOfBytesRead = 0;
+                        do
+                        {
+                            numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+                            myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+                        }
+                        while (stream.DataAvailable);
+
+                        result = myCompleteMessage.ToString();
+                        return result != null;
+                    }
+                return false;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
         }
 
         public bool ReadWeightData(SerialPort channel, out string result)
         {
             result = null;
-            return false;
+            try
+            {
+                if (channel == null)
+                    return false;
+                if (channel.IsOpen && channel.BytesToRead > 0)
+                {
+
+                    byte[] myReadBuffer = new byte[1024];
+                    int numberOfBytesRead = 0;
+                    numberOfBytesRead = channel.Read(myReadBuffer, 0, myReadBuffer.Length);
+                    result = string.Format("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+                    return result != null;
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
         }
+    
 
         public Tele3xxxEDV ConvertToEDV(string result)
         {
@@ -29,12 +79,23 @@ namespace soehnle.mes.processapplication
 
         public bool StartReadingWeights(TcpClient channel)
         {
-            return false;
+            if (channel == null || !channel.Connected)
+                return false;
+            NetworkStream stream = channel.GetStream();
+            if (stream == null || !stream.CanWrite)
+                return false;
+            stream.Write(Cmd3xxx.GetValueAllTime, 0, Cmd3xxx.C_CmdLength);
+            return true;
+
         }
 
         public bool StartReadingWeights(SerialPort channel)
         {
-            return false;
+            if (!channel.IsOpen)
+                return false;
+            channel.Write(Cmd3xxx.GetValueAllTime, 0, Cmd3xxx.C_CmdLength);
+            return true;
+
         }
         #endregion
     }
