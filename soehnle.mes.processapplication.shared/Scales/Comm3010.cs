2b52aa555124e4c35d1c6002a30662ecf8c79575
Damir Lisak <dlisak@kajbum.com>
2022-01-05 19:01:01 +0100

    Soehnle Communication unified - untested

--- soehnle.mes.processapplication.shared/Scales/Comm3010.cs
+++ soehnle.mes.processapplication.shared/Scales/Comm3010.cs
@@ -10,6 +10,24 @@ namespace soehnle.mes.processapplication
     public class Comm3010 : Comm3xxxBase
     {
         #region Methods
+        public override bool SendReadAlibiCmd(TcpClient channel)
+        {
+            if (channel == null || !channel.Connected)
+                return false;
+            NetworkStream stream = channel.GetStream();
+            if (stream == null || !stream.CanWrite)
+                return false;
+            stream.Write(Cmd3xxx.GetValueOneTimeStill, 0, Cmd3xxx.C_CmdLength);
+            return true;
+        }
+
+        public override bool SendReadAlibiCmd(SerialPort channel)
+        {
+            if (!channel.IsOpen)
+                return false;
+            channel.Write(Cmd3xxx.GetValueOneTimeStill, 0, Cmd3xxx.C_CmdLength);
+            return true;
+        }
         #endregion
     }
 }
--- soehnle.mes.processapplication.shared/Scales/Comm3xxxBase.cs
+++ soehnle.mes.processapplication.shared/Scales/Comm3xxxBase.cs
@@ -11,71 +11,49 @@ namespace soehnle.mes.processapplication
     {
         #region Methods
         public bool ReadWeightData(TcpClient channel, out string result)
-        {
-            
+        {            
             result = null;
             if (channel == null || !channel.Connected)
                 return false;
-            try
-            {
-                    NetworkStream stream = channel.GetStream();
-                    if (stream == null)
-                        return false;
-                    if (stream.CanRead && stream.DataAvailable)
-                    {
-                        byte[] myReadBuffer = new byte[1024];
-                        StringBuilder myCompleteMessage = new StringBuilder();
-                        int numberOfBytesRead = 0;
-                        do
-                        {
-                            numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
-                            myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
-                        }
-                        while (stream.DataAvailable);
-
-                        result = myCompleteMessage.ToString();
-                        return result != null;
-                    }
+            NetworkStream stream = channel.GetStream();
+            if (stream == null)
                 return false;
-
-            }
-            catch (Exception e)
+            if (stream.CanRead && stream.DataAvailable)
             {
-                return false;
-            }
+                byte[] myReadBuffer = new byte[1024];
+                StringBuilder myCompleteMessage = new StringBuilder();
+                int numberOfBytesRead = 0;
+                do
+                {
+                    numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+                    myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+                }
+                while (stream.DataAvailable);
 
+                result = myCompleteMessage.ToString();
+                return result != null;
+            }
+            return false;
         }
+
 
         public bool ReadWeightData(SerialPort channel, out string result)
         {
             result = null;
-            try
-            {
-                if (channel == null)
-                    return false;
-                if (channel.IsOpen && channel.BytesToRead > 0)
-                {
-
-                    byte[] myReadBuffer = new byte[1024];
-                    int numberOfBytesRead = 0;
-                    numberOfBytesRead = channel.Read(myReadBuffer, 0, myReadBuffer.Length);
-                    result = string.Format("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
-                    return result != null;
-                }
+            if (channel == null)
                 return false;
-            }
-            catch (Exception e)
+            if (channel.IsOpen && channel.BytesToRead > 0)
             {
-                return false;
-            }
 
+                byte[] myReadBuffer = new byte[1024];
+                int numberOfBytesRead = 0;
+                numberOfBytesRead = channel.Read(myReadBuffer, 0, myReadBuffer.Length);
+                result = string.Format("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+                return result != null;
+            }
+            return false;
         }
     
-
-        public Tele3xxxEDV ConvertToEDV(string result)
-        {
-            return new Tele3xxxEDV(result);
-        }
 
         public bool StartReadingWeights(TcpClient channel)
         {
@@ -86,8 +64,8 @@ namespace soehnle.mes.processapplication
                 return false;
             stream.Write(Cmd3xxx.GetValueAllTime, 0, Cmd3xxx.C_CmdLength);
             return true;
-
         }
+
 
         public bool StartReadingWeights(SerialPort channel)
         {
@@ -95,8 +73,56 @@ namespace soehnle.mes.processapplication
                 return false;
             channel.Write(Cmd3xxx.GetValueAllTime, 0, Cmd3xxx.C_CmdLength);
             return true;
-
         }
+
+
+        public bool StopReadingWeights(TcpClient channel)
+        {
+            if (channel == null || !channel.Connected)
+                return false;
+            NetworkStream stream = channel.GetStream();
+            if (stream == null || !stream.CanWrite)
+                return false;
+            stream.Write(Cmd3xxx.GetValueOneTime, 0, Cmd3xxx.C_CmdLength);
+            return true;
+        }
+
+
+        public bool StopReadingWeights(SerialPort channel)
+        {
+            if (!channel.IsOpen)
+                return false;
+            channel.Write(Cmd3xxx.GetValueOneTime, 0, Cmd3xxx.C_CmdLength);
+            return true;
+        }
+
+
+        public virtual bool SendReadAlibiCmd(TcpClient channel)
+        {
+            if (channel == null || !channel.Connected)
+                return false;
+            NetworkStream stream = channel.GetStream();
+            if (stream == null || !stream.CanWrite)
+                return false;
+            stream.Write(Cmd3xxx.GetValueOneTimeStillWithAlibi, 0, Cmd3xxx.C_CmdLength);
+            return true;
+        }
+
+
+        public virtual bool SendReadAlibiCmd(SerialPort channel)
+        {
+            if (!channel.IsOpen)
+                return false;
+            channel.Write(Cmd3xxx.GetValueOneTimeStillWithAlibi, 0, Cmd3xxx.C_CmdLength);
+            return true;
+        }
+
+
+        public Tele3xxxEDV ConvertToEDV(string result)
+        {
+            return new Tele3xxxEDV(result);
+        }
+
         #endregion
     }
 }
