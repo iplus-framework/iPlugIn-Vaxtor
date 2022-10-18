8d623db80bb3b489ed22b1e826f77ff8c381fdf8
aagincic <aleksandar.agincic@gmail.com>
2022-10-18 09:14:05 +0200

    setup advantech.mes.processapplication

---
+++ advantech.mes.processapplication/Model/LogMsg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace advantech.mes.processapplication
+{
+    public class LogMsg
+    {
+        public int PE { get; set; }
+        public string UID { get; set; }
+        public DateTime TIM { get; set; }
+
+        public int[][] Record { get; set; }
+    }
+}
