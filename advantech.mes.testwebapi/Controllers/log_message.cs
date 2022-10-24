88b909f915f06650708ac85f21ad502dd541ee9c
aagincic <aleksandar.agincic@gmail.com>
2022-10-24 15:52:58 +0200

    testwebapi - test tool for testing PAEWise4000

---
+++ advantech.mes.testwebapi/Controllers/log_message.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace advantech.mes.testwebapi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class log_message : Controller
+    {
+
+        [HttpGet(Name = "log_message_get")]
+        public IActionResult Get()
+        {
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string name = assembly.GetManifestResourceNames().SingleOrDefault(n => n.EndsWith("log_message.json", StringComparison.InvariantCultureIgnoreCase));
+            using Stream stream = assembly.GetManifestResourceStream(name);
+            StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+            var content = reader.ReadToEnd();
+            return Content(content, "text/json", Encoding.UTF8);
+        }
+    }
+}
---
+++ advantech.mes.testwebapi/Model/Wise4000Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advantech.mes.processapplication
+{
+    public class Wise4000Data
+    {
+        public LogMsg[] LogMsg { get;set;}
+    }
+}
